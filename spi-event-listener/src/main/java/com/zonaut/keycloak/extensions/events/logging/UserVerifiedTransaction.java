f9db5b3c138cc18ca2bb826bb9586e66fb5b86e9
zonaut <info@zonaut.com>
2020-09-26 10:22:45 +0200

    extended the even listener example with a transactional http call

--- spi-event-listener/src/main/java/com/zonaut/keycloak/extensions/events/logging/UserVerifiedTransaction.java
+++ spi-event-listener/src/main/java/com/zonaut/keycloak/extensions/events/logging/UserVerifiedTransaction.java
@@ -1,11 +1,29 @@
 package com.zonaut.keycloak.extensions.events.logging;
 
+import static javax.ws.rs.core.HttpHeaders.CONTENT_TYPE;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 import org.jboss.logging.Logger;
 import org.keycloak.models.AbstractKeycloakTransaction;
+import org.keycloak.utils.MediaType;
 
 public class UserVerifiedTransaction extends AbstractKeycloakTransaction {
 
     private static final Logger log = Logger.getLogger(UserVerifiedTransaction.class);
+
+    private static final HttpClient HTTP_CLIENT = HttpClient.newBuilder()
+        .version(HttpClient.Version.HTTP_2)
+        .connectTimeout(Duration.ofSeconds(3))
+        .build();
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     private final UserUuidDto userUuidDto;
 
@@ -23,6 +41,35 @@ public class UserVerifiedTransaction extends AbstractKeycloakTransaction {
         // You could make a http call here and send the object.
         // When we throw an exception here, the user would not be verified when using .enlistPrepare
         //throw new RuntimeException("External call failed!");
+
+        try {
+
+            HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(OBJECT_MAPPER.writeValueAsString(this.userUuidDto)))
+                .uri(URI.create("https://httpbin.org/post"))
+                .header(CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .build();
+
+            HttpResponse<String> response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+
+            log.info("#### STATUS CODE");
+            log.info(response.statusCode());
+            if (response.statusCode() != 200) {
+                throw new RuntimeException("##### WRONG RESPONSE STATUS CODE !!! : " + response.statusCode());
+            }
+
+            log.info("### USER VERIFIED TRANSACTION SUCCESS");
+
+            log.info("#### HEADERS");
+            HttpHeaders headers = response.headers();
+            headers.map().forEach((k, v) -> log.info("##### " + k + ":" + v));
+
+            log.info("#### RESPONSE BODY");
+            log.info(response.body());
+
+        } catch (Exception e) {
+            throw new RuntimeException("##### USER VERIFIED TRANSACTION FAILED !", e);
+        }
     }
 
     @Override
